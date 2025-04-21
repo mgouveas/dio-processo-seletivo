24f503999aed54800fe2afcabd190fe67caac922
mgouveas <mgsandmgs@gmail.com>
2025-04-21 18:23:28 -0300

    feature: Desenvolvimento do app de análise de candidatos

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,78 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Sistema de Processo Seletivo");
+
+        System.out.println("Analisando candidatos. Aguarde...");
+        selecionaCandidato();
     }
+
+    public static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1700, 2300);
+    }
+
+    public static boolean aceitaProposta() {
+        Random random = new Random();
+        return random.nextBoolean();
+    }
+
+    public static ArrayList<String> adicionaCandidato(String candidato) {
+        ArrayList<String> candidatosAprovados = new ArrayList<>();
+        candidatosAprovados.add(candidato);
+        return candidatosAprovados;
+    }
+
+    public static boolean analisaCandidatos(double salarioPretendido, double salarioBase, String candidato) {
+        if (salarioBase > salarioPretendido ) {
+            System.out.println("Ligar para o Candidato");
+            System.out.println("Candidato Selecionado");
+            adicionaCandidato(candidato);
+            return true;
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("Ligar para o Candidato com uma contra proposta");
+            if (aceitaProposta()){
+                System.out.println("Proposta Aceita. Candidato Selecionado");
+                adicionaCandidato(candidato);
+                return true;
+            }
+        } else {
+            System.out.println("Aguardar o resultado dos demais candidatos.");
+        }
+        return false;
+    }
+
+    public static void selecionaCandidato() {
+        System.out.println("Recebendo lista de candidatos.");
+        String[] candidatos = {
+                "Alice", "Bruno", "Carla", "Daniel", "Eduarda",
+                "Fernando", "Gabriela", "Henrique", "Isabela", "João",
+                "Karen", "Leonardo", "Mariana", "Natália", "Otávio"
+        };
+
+        double salarioBase = 2000.00;
+        ArrayList<String> candidatosAprovados = new ArrayList<>();
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
+            for (String candidato: candidatos){
+                System.out.println("Analisando o(a) candidato(a): " + candidato);
+                double salarioPretendido = valorPretendido();
+                if (analisaCandidatos(salarioPretendido, salarioBase, candidato)) {
+                    candidatosAprovados.add(candidato);
+                    candidatosSelecionados++;
+                }
+
+                candidatoAtual++;
+            }
+        }
+
+        System.out.println("Candidatos Selecionados: " + candidatosAprovados);
+
+    }
+
 }
